a47bbf03d4cb35919d1227debb2f5065ea004ee4
jonascampos1 <jonascampos1@hotmail.com>
2023-01-31 15:38:36 -0600

    Sqlalchemy again

--- services/user.py
+++ services/user.py
@@ -1,27 +1,14 @@
 from models.user import userTable
 from config.db import conn
 from models.user import User
-import pymysql
-import os
 
-try:
-    conn2 = pymysql.connect(
-        user=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        host=os.getenv('MYSQL_HOST'),
-        database=os.getenv('MYSQL_DATABASE'),
-    )
-except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
-	print("Error db no connect: ", e)
-
-cursor =  conn2.cursor()
 
 def get_users():
     r = conn.execute(userTable.select()).fetchall()
     return r
 
 
-def create_user(user):
+def create_user(user: User):
     resultc = conn.execute(userTable.insert().values(user))
     return conn.execute(userTable.select().where(userTable.c.id == resultc.lastrowid)).first()
 
@@ -31,13 +18,7 @@ def verify_user_exist(username):
     return resultv
 
 def getBalance_(id):
-    sql='SELECT balance FROM User WHERE id='+str(id)
-    cursor.execute(sql)
-    print(sql)
-    resultb= cursor.fetchone()
-    #resultb = conn.execute(userTable.select().where(userTable.c.id == id)).first()
-    for e in resultb:
-        return e
     
-    
+    resultb = conn.execute(userTable.select().where(userTable.c.id == id)).first()
+    return int(resultb['balance'])
     
