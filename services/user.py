01633a706242ab0bfd6a7a59a8f692ab4e0e082b
jonascampos1 <jonascampos1@hotmail.com>
2023-01-31 15:27:30 -0600

    Try use rawsql for getBalance_

--- services/user.py
+++ services/user.py
@@ -1,14 +1,27 @@
 from models.user import userTable
 from config.db import conn
 from models.user import User
+import pymysql
+import os
 
+try:
+    conn2 = pymysql.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        database=os.getenv('MYSQL_DATABASE'),
+    )
+except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
+	print("Error db no connect: ", e)
+
+cursor =  conn2.cursor()
 
 def get_users():
     r = conn.execute(userTable.select()).fetchall()
     return r
 
 
-def create_user(user: User):
+def create_user(user):
     resultc = conn.execute(userTable.insert().values(user))
     return conn.execute(userTable.select().where(userTable.c.id == resultc.lastrowid)).first()
 
@@ -18,8 +31,11 @@ def verify_user_exist(username):
     return resultv
 
 def getBalance_(id):
+    cursor.execute('SELECT balance FROM User WHERE id='+str(id))
+    resultb= cursor.fetchone()
+    #resultb = conn.execute(userTable.select().where(userTable.c.id == id)).first()
+    for e in resultb:
+        return e
     
-    resultb = conn.execute(userTable.select().where(userTable.c.id == id)).first()
-    print(resultb)
-    return int(resultb['balance'])
+    
     
