f1d9bc104908a1f6bd3d459ae1cd9bfcf3c15077
jonascampos1 <jonascampos1@hotmail.com>
2023-02-03 07:42:03 -0600

    Clean code and ad create statement on init_db.py

--- init_db.py
+++ init_db.py
@@ -1,34 +1,65 @@
-from config.db import conn
-from models.user import userTable
-from models.calculator import operationTable
+from config.pymysql_db import connect_mysql
 import hashlib
 
 password1_hashed = hashlib.md5('aB12345@'.encode('utf8')).hexdigest()
 password2_hashed = hashlib.md5('aB12345#'.encode('utf8')).hexdigest()
 
-conn.execute('TRUNCATE TABLE User')
-conn.execute('TRUNCATE TABLE Operation')
-conn.execute('TRUNCATE TABLE Record')
+connection = connect_mysql()
+with connection:
+    with connection.cursor() as cursor:
 
-user = [
-    {'username': 'test1@test.com', 'password': password1_hashed, 'status': 0, 'balance': 30},
-    {'username': 'test2@test.com', 'password': password2_hashed, 'status': 0, 'balance': 30},
-    ]
+        cursor.execute('CREATE TABLE `Operation` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `type` varchar(30) DEFAULT NULL,\
+        `cost` float DEFAULT NULL,\
+        PRIMARY KEY (`id`)\
+        ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;\
+        ')
+        cursor.execute('CREATE TABLE `Record` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `operation_id` int(11) DEFAULT NULL,\
+        `user_id` int(11) DEFAULT NULL,\
+        `amount` float DEFAULT NULL,\
+        `user_balance` float DEFAULT NULL,\
+        `operation_response` varchar(30) DEFAULT NULL,\
+        `date` datetime DEFAULT NULL,\
+        `deletedAt` datetime DEFAULT NULL,\
+        PRIMARY KEY (`id`)\
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;\
+        ')
 
-result = conn.execute(userTable.insert().values(user))
+        cursor.execute('CREATE TABLE `User` (\
+        `id` int(11) NOT NULL AUTO_INCREMENT,\
+        `username` varchar(30) DEFAULT NULL,\
+        `password` varchar(50) DEFAULT NULL,\
+        `status` smallint(6) DEFAULT NULL,\
+        `balance` float DEFAULT NULL,\
+        PRIMARY KEY (`id`)\
+        ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;\
+        ')
+
+        sql = "INSERT INTO `Operation` (\
+                type,\
+                cost\
+                ) \
+                VALUES(%s,%s)"
+        cursor.execute(sql, ('addition','1'))
+        cursor.execute(sql, ('substraction','2'))
+        cursor.execute(sql, ('multiplication','3'))
+        cursor.execute(sql, ('division','4'))
+        cursor.execute(sql, ('square_root','5'))
+        cursor.execute(sql, ('random_string','6'))
+
+        sql = "INSERT INTO `User` (\
+                username,\
+                password,\
+                status,\
+                balance\
+                ) \
+                VALUES(%s,%s,%s,%s)"
+        cursor.execute(sql, ('test1@test.com',password1_hashed,'0','30'))
+        cursor.execute(sql, ('test2@test.com',password2_hashed,'0','30'))
+       
+    connection.commit()
 
 
-operation = [
-    {'type':'addition', 'cost':1},
-    {'type':'substraction', 'cost':2},
-    {'type':'multiplication', 'cost':3},
-    {'type':'division', 'cost':4},
-    {'type':'square_root', 'cost':5},
-    {'type':'random_string', 'cost':6}
-    ]
-
-result = conn.execute(operationTable.insert().values(operation))
-
-
-
-conn.close()
--- models/user.py
+++ models/user.py
@@ -1,18 +1,6 @@
-from sqlalchemy import Table, Column
-from config.db import meta, engine
-from sqlalchemy.sql.sqltypes import INTEGER, VARCHAR, SMALLINT, FLOAT
 from typing import Optional
 from pydantic import BaseModel
 
-userTable = Table("User", meta,
-                  Column("id", INTEGER, primary_key=True),
-                  Column("username", VARCHAR(30)),
-                  Column("password", VARCHAR(50)),
-                  Column('status', SMALLINT),
-                  Column('balance', FLOAT)
-                )
-
-meta.create_all(engine)
 
 class User(BaseModel):
     id: Optional[str]
